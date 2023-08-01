da18d81b8aaca00246999dc0d4e8bc53f29a497d
Rodrigo Martínez <60165811+RodrigoMartinez10@users.noreply.github.com>
2023-08-01 15:38:19 -0600

    Update streamlit_app.py
    Correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.subtitle('Breakfast Menu')
+streamlit.subheader('Breakfast Menu')
 streamlit.write('Omega 3 & Blueberry Oatmeal')
